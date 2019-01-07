ac1265720b3d7352a3dffb931dffe3a2999622f1
hitesh9811 <33113705+hitesh9811@users.noreply.github.com>
2019-01-07 05:13:02 -0100

    Add files via upload

---
+++ tri.pl
@@ -0,0 +1,28 @@
+male(hitesh).
+male(khushvinder).
+male(ram).
+female(kavita).
+female(kasturba bai).
+female(manikarnika).
+female(padmavati).
+male(aurangzeb).
+male(shah_jahan).
+female(mumtaz).
+male(bhavin).
+male(pawan).
+male(naveen).
+female(arti).
+female(sangeeta).
+female(jodha).
+female(sara_ali_khan).
+male(kartik_aryan).
+male(taimur_kapoor_khan). 
+male(anurag_chu_h).
+parent(khushvinder,hitesh).
+father(X,Y):- male(X), parent(X,Y).
+mother(kavita,hitesh).
+
+
+
+
+
